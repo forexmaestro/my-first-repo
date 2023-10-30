feab9935f88870097cf384aff4d7460b63fafb26
forexmaestro <149265861+forexmaestro@users.noreply.github.com>
2023-10-30 14:00:38 -0400

    Update test.js

--- test.js
+++ test.js
@@ -8,7 +8,7 @@ function multi(arg){
       output: process.stdout
     });
 
-    rl.question(`Which number would you like to multiply by ${arg}? `, function(newarg) {
+    rl.question(`Enter number would you like to multiply by ${arg}? `, function(newarg) {
       let result = parseInt(newarg) * arg;
       console.log(result);
       rl.close();
