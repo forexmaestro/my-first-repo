f82d1d896196154e743d9e6bfef049a4581d3d80
Travis <travis.williams3@student.uagc.edu>
2023-10-30 13:37:27 -0400

    This is a JS script

---
+++ test.js
@@ -0,0 +1,21 @@
+const arg = parseInt(process.argv[2]);
+
+function multi(arg){
+  if (!isNaN(arg)) {
+    const readline = require('readline');
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+
+    rl.question(`What is the number you would like to multiply by ${arg}? `, function(newarg) {
+      let result = parseInt(newarg) * arg;
+      console.log(result);
+      rl.close();
+    });
+  } else {
+    console.log("Please provide a valid number.");
+  }
+}
+
+multi(arg);
