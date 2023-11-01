1c16f9e4fae742769d42bc661c4e12953f69cfc0
forexmaestro <149265861+forexmaestro@users.noreply.github.com>
2023-11-01 06:12:38 -0400

    Update lol.sh

--- lol.sh
+++ lol.sh
@@ -3,6 +3,7 @@ echo "Hello World, Lol"
 
 echo "Talk about Remiss, lol"
 echo "How have you been?"
-=======
+
 echo "Hello World, Lol tf you doin?"
->>>>>>> cececebf383555212e91f231f5bcbc204d393ec8
+
+Let's clean this up a bit for the PULL
